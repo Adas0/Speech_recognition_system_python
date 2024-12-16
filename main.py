73c52689411bf6fcd94b50eb046ac8dd96486989
Adas0 <42966498+Adas0@users.noreply.github.com>
2024-12-16 17:22:44 +0100

    Code cleanup

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#Date: 2018
+#Author: Adam Korytowski
+
 from numpy.linalg import norm
 import scipy.signal as sig
 from scipy.io import wavfile
@@ -9,22 +12,17 @@ import glob
 import os
 from dtw import dtw
 
-
 sample_rate, audio_wiosna = wavfile.read("./wiosna-Adam-Korytowski.wav")
 sample_rate_, audio_lato = wavfile.read("./zima-Adam-Korytowski.wav")
 sample_rate__, audio_jesien = wavfile.read("./jesien-Adam-Korytowski.wav")
 sample_rateasd, audio = wavfile.read("./jeden_16bit.wav")
-# sample_rate, audio = wavfile.read("./zima-Adam-Korytowski.wav")
-
 
 frame_time = 0.02
 nftt = frame_time * sample_rate
 
-
 def normalize_signal(audio_):
     audio_ = audio_ / np.max(np.abs(audio_))
     return audio_
-
 
 def frame_and_window_signal(signal, frame_time=0.02):
     frame_size = int(frame_time * sample_rate)  # number of samples in frame
@@ -38,7 +36,6 @@ def frame_and_window_signal(signal, frame_time=0.02):
     frames = list(frames.values())
 
     windowed_frames = window_frames(frames)
-    # fft_frames = fft.fft(windowed_frames)
     fft_frames = list()
     for el in windowed_frames:
         fft_frames.append(getFFT(el))
@@ -147,7 +144,6 @@ def trigger_plots(filtered_audio, framed_audio):
     plt.show()
 
 
-# ta funkcja poprawnie przelicza, potwierdzone
 def freq_to_mel(freq):
     return 1125 * np.log(1 + freq/700)
 
@@ -158,14 +154,12 @@ def mel_to_freq(mel_freqs):
         freqs.append(700 * (np.exp(mel_freqs[i]/1125)-1))
     return freqs
 
-
 def round_freqs(freqs):
     rounded_freqs = list()
     for el in freqs:
         rounded_freqs.append(np.floor((nftt + 1) * 2 * el / sample_rate))
 
     return rounded_freqs
-
 
 def calculate_filter_middle_freqs(low_freq, high_freq, filters_number):
     low_mel_freq = freq_to_mel(low_freq)
@@ -177,14 +171,12 @@ def calculate_filter_middle_freqs(low_freq, high_freq, filters_number):
     freqs = round_freqs(freqs)
     return freqs
 
-
 def generate_filter_bank():
     low_freq = 80
     high_freq = 4000
     filters_number = 20
 
     middle_freqs = calculate_filter_middle_freqs(low_freq, high_freq, filters_number)
-    # fbank = np.zeros(filters_number, int(np.floor(nftt/2 + 1)))
     fbank = np.zeros((filters_number, int(np.floor(nftt ))))
     for m in range(1, filters_number + 1):
         f_m_minus = int(middle_freqs[m - 1])
@@ -244,7 +236,6 @@ def get_MFCC(audio):
     filter_bank = generate_filter_bank()
 
     for i in range(0, len(filter_bank)):    # numer filtru
-        # next((i for i, x in enumerate(filter_bank[i]) if x), None)
         for j in range(0, len(filter_bank[0])):     # numer elementu w filtrze
             if filter_bank[i][j] == 0:
                 filter_bank[i][j] = 1
@@ -259,20 +250,6 @@ def get_MFCC(audio):
             filtered += el * frame
         new_frames.append(filtered)
 
-    # MFCC = new_frames
-    # matrix
-
-
-
-    # matrix = scipy.fftpack.dct(new_frames)
-    # to jest macierz (ramki x probki w tych ramkach). czyli jakieś 263x160. (260 ramek, każda po 160 próbek)
-    # ma powstać: 263 x 20. Czyli z tego 160 trzeba zrobić 20. Jak? - Trzeba uśrednić wartości w pasmach melowych:
-    # czyli: patrzysz gdzie się zaczyna i kończy pierwszy filtr : zaczyna się w indeksie 4, kończy w 8. więc bierzesz
-    # wartości od 4 do 8 i robisz z niech średnią. Ta średnia to pierwszy element. Średnia z drugiego pasma to będzie
-    # drugi element, etc, aż do dwudziestego filtra. (Jak otworzysz sobie zmienna filter_bank to tam widac gdzie
-    # dokladnie sie zaczynaja i koncza te filtry)
-    # ta macierz to bedzie już poprawne mfcc
-
     new_frames = np.matrix(new_frames)
     MFCC = matrix_sum(new_frames)
     MFCC = 20 * np.log10(MFCC)
@@ -286,8 +263,6 @@ def get_MFCC(audio):
 
 
 mfcc = get_MFCC(audio)
-
-
 
 def get_mfcc_pattern(word):
     MFCCs = list()
@@ -324,7 +299,6 @@ def get_distances(word):
 
     MFCC_tested_word = get_MFCC(audio_lato)
     word_files = list()
-    # dcts = list()
     for file in glob.glob(word + "*.wav"):
         word_files.append(file)
 
@@ -337,27 +311,4 @@ def get_distances(word):
 
     return dist
 
-
-
 words = ["wiosna", "lato", "jesień", "zima", "poniedziałek", "wtorek", "środa", "czwartek", "piątek", "sobota", "niedziela"]
-
-# wiosna = list()
-# os.chdir("./pory_roku_8k/")
-# wiosna.append(get_distances("wiosna"))
-# wiosna.append(get_distances("lato"))
-# wiosna.append(get_distances("jesień"))
-# wiosna.append(get_distances("zima"))
-#
-# os.chdir("..")
-# os.chdir("./dni_tygodnia_8k/")
-# wiosna.append(get_distances("poniedziałek"))
-# wiosna.append(get_distances("wtorek"))
-# wiosna.append(get_distances("środa"))
-# wiosna.append(get_distances("czwartek"))
-# wiosna.append(get_distances("piątek"))
-# wiosna.append(get_distances("sobota"))
-# wiosna.append(get_distances("niedziela"))
-#
-#
-# print(wiosna)
-
